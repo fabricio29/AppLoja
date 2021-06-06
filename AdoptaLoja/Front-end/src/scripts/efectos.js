071f469982ea9943483aff10aca121a225b38dfb
LeninFabricio <yanangomezfabricio@gmail.com>
2021-06-06 17:36:17 -0500

    FirstConmint
    Inicio

---
+++ AdoptaLoja/Front-end/src/scripts/efectos.js
@@ -0,0 +1,50 @@
+$(document).ready(function(){
+
+    /* nav-bar active responsi */
+
+    
+    // owl carousel script
+    $('.carousel').owlCarousel({
+        margin: 20,
+        loop: true,
+        autoplayTimeOut: 2000,
+        autoplayHoverPause: true,
+        responsive: {
+            0:{
+                items: 1,
+                nav: false
+            },
+            600:{
+                items: 2,
+                nav: false
+            },
+            1000:{
+                items: 3,
+                nav: false
+            }
+        }
+    });
+
+});
+
+
+const body = document.querySelector("body");
+const navbar = document.querySelector(".navbar");
+const menu = document.querySelector(".menu-list");
+const menuBtn = document.querySelector(".menu-btn");
+const cancelBtn = document.querySelector(".cancel-btn");
+menuBtn.onclick = ()=>{
+    menu.classList.add("active");
+    menuBtn.classList.add("hide");
+    body.classList.add("disabledScroll");
+
+}
+
+cancelBtn.onclick = ()=>{
+    menu.classList.remove("active");
+    menuBtn.classList.remove("hide");
+    body.classList.remove("disabledScroll");
+
+}
+
+window
